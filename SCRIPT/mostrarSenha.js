633dc3958e000c3b454dba26abfa7893cb0d0263
charliehenrikis <134668945+charliehenrikis@users.noreply.github.com>
2023-09-26 23:08:49 -0300

    Criação da tela de login e começo da tela principal

---
+++ SCRIPT/mostrarSenha.js
@@ -0,0 +1,8 @@
+function mostrarOcultarSenha() {
+    const senha=document.getElementById("senha")
+    if(senha.type=="password") {
+        senha.type="text"
+    } else {
+        senha.type="password"; 
+    }
+}
